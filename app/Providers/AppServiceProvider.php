8481527d4e45f16831a714b5772007f3ac21599c
Jhoan Borges <jhoan.borges@hexagun.mx>
2025-08-05 16:39:53 -0600

    address breeze

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,7 @@
 
 namespace App\Providers;
 
-use Essa\APIToolKit\Exceptions\Handler;
-use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(ExceptionHandler::class, Handler::class);
+        //
     }
 
     /**
@@ -21,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
+            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        });
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,8 @@
 <?php
 
-use App\Http\Controllers\TareasController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
-
-// Route::apiResource('tareas', TareasController::class);
-
-Route::get('/tareas', [TareasController::class, 'index']);
-Route::post('/tareas', [TareasController::class, 'store']);
-Route::put('/tareas/{id}', [TareasController::class, 'update']);
-Route::delete('/tareas/{id}', [TareasController::class, 'destroy']);
+});
