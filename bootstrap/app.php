1d902fbe6d2bfdd28588fb8b8fd91aa0ef19d213
Jhoan Borges <jhoan.borges@hexagun.mx>
2025-08-05 17:25:56 -0600

    added custom api for authentication

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,13 @@ return Application::configure(basePath: dirname(__DIR__))
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
 
+        $middleware->validateCsrfTokens(except: [
+            'login',
+            'register',
+            'password/reset',
+            'api/*',
+        ]);
+
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
